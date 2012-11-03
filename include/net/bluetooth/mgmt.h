87fc11f10870041fd15d6e897e0b9644d3cb7ec1
yozgatg <yozgatg@UbuntuVM.(none)>
2012-11-03 10:44:32 +0100

    Revert "bluetooth: LE conn whitelist implementation"
    This reverts commit b9c73b3e31cb1c3411d14e3780bea0a855ac8420.

--- include/net/bluetooth/mgmt.h
+++ include/net/bluetooth/mgmt.h
@@ -60,7 +60,6 @@ struct mgmt_rp_read_info {
 	__u8 hci_ver;
 	__u16 hci_rev;
 	__u8 name[MGMT_MAX_NAME_LENGTH];
-	__u8 le_white_list_size;
 } __packed;
 
 struct mgmt_mode {
@@ -234,39 +233,6 @@ struct mgmt_cp_encrypt_link {
 	bdaddr_t bdaddr;
 	__u8 enable;
 } __packed;
-
-#define MGMT_OP_SET_RSSI_REPORTER		0x0022
-struct mgmt_cp_set_rssi_reporter {
-	bdaddr_t	bdaddr;
-	__s8		rssi_threshold;
-	__le16	interval;
-	__u8		updateOnThreshExceed;
-} __packed;
-
-#define MGMT_OP_UNSET_RSSI_REPORTER		0x0023
-struct mgmt_cp_unset_rssi_reporter {
-	bdaddr_t	bdaddr;
-} __packed;
-
-#define MGMT_OP_LE_READ_WHITE_LIST_SIZE	0xE000
-
-#define MGMT_OP_LE_CLEAR_WHITE_LIST	0xE001
-
-#define MGMT_OP_LE_ADD_DEV_WHITE_LIST	0xE002
-struct mgmt_cp_le_add_dev_white_list {
-	__u8 addr_type;
-	bdaddr_t bdaddr;
-} __packed;
-
-#define MGMT_OP_LE_REMOVE_DEV_WHITE_LIST	0xE003
-struct mgmt_cp_le_remove_dev_white_list {
-	__u8 addr_type;
-	bdaddr_t bdaddr;
-} __packed;
-
-#define MGMT_OP_LE_CREATE_CONN_WHITE_LIST	0xE004
-
-#define MGMT_OP_LE_CANCEL_CREATE_CONN_WHITE_LIST	0xE005
 
 #define MGMT_EV_CMD_COMPLETE		0x0001
 struct mgmt_ev_cmd_complete {
